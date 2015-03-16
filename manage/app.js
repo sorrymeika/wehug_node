9caa63cf1112d8b82c24fd689a4826d7762c051f
sun.lu <sun.lu@Ctrip.com>
2015-03-16 17:27:19 +0800

    node first

---
+++ manage/app.js
@@ -0,0 +1,12 @@
+﻿var express=require('express');
+var app=express();
+var args=process.argv;
+var port=args.length>=3?parseInt(args[2]):5555;
+
+console.log(process.argv)
+
+app.get('/',function(req,res) {
+    res.send('hello world');
+});
+
+app.listen(port);
