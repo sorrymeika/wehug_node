9188b618f36b5c96baaead058324ad3e140cd103
sorrymeika <sorrymeika@163.com>
2015-04-26 23:17:50 +0800

    fx

--- manage/app.js
+++ manage/app.js
@@ -3,10 +3,20 @@ var app=express();
 var args=process.argv;
 var port=args.length>=3?parseInt(args[2]):5555;
 
-console.log(process.argv)
+console.log("start with args",process.argv)
 
-app.get('/',function(req,res) {
+app.get('/',function (req,res) {
+
+    var MongoClient=require('mongodb').MongoClient;
+
+    var url='mongodb://192.168.0.106:27017/test';
+
+    MongoClient.connect(url,function (err,db) {
+        console.log("Connected correctly to server.");
+        db.close();
+    });
+
     res.send('hello world');
 });
 
-app.listen(port);
+app.listen(port,"127.0.0.1");
