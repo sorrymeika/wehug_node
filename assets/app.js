1f1795939d7ec2cef3409f64f3f329a263ce2b7d
sun.lu <sun.lu@Ctrip.com>
2015-05-27 14:08:33 +0800

    fx

--- assets/app.js
+++ assets/app.js
@@ -10,10 +10,10 @@ var tools=new Tools(path.join(__dirname,'./'),path.join(__dirname,'./dest'));
 var razor=require('./../core/razor');
 var fs=require('fs');
 
-app.get('/js/template/*.js',function (req,res) {
+app.get('/js/template/*.js',function(req,res) {
     fs.readFile('./template/'+req.params[0]+'.tpl',{
         encoding: 'utf-8'
-    },function (err,text) {
+    },function(err,text) {
 
         text=tools.compressJs(razor.web(text));
         res.set('Content-Type','text/javascript');
@@ -22,76 +22,7 @@ app.get('/js/template/*.js',function (req,res) {
 });
 
 
-app.get('/test',function (req,res) {
-
-    var Canvas=require('canvas'),
-        height=50,
-        Image=Canvas.Image,
-        canvas=new Canvas(100,50),
-        ctx=canvas.getContext('2d');
-
-    var util=require('./../core/util');
-
-    var font=['Impact','Arial'];
-    var words=["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z","2","3","4","5","6","7","8","9"];
-
-    var result='';
-    var gradient=ctx.createLinearGradient(0,0,50,0);
-    gradient.addColorStop(0,"magenta");
-    gradient.addColorStop(0.5,"blue");
-    gradient.addColorStop(1.0,"red");
-
-    for(var i=0;i<4;i++) {
-        var fontSize=util.random(35,40);
-        var fontFamily=util.random(0,1);
-        var rotate=.01*util.random(-45,45);
-
-        var wordCanvas=new Canvas(50,50),
-        wordCtx=wordCanvas.getContext('2d');
-
-        wordCtx.rotate(rotate);
-        wordCtx.font=fontSize+'px '+font[fontFamily];
-        var c=words[util.random(55)];
-        result+=c;
-
-        // 用渐变进行填充
-        wordCtx.strokeStyle=gradient;
-
-        wordCtx.strokeText(c,rotate>0.15?rotate*50:5,40);
-
-        var img=new Image;
-        img.src=wordCanvas.toBuffer();
-
-        ctx.drawImage(img,i*20,0,50,50)
-    }
-
-    gradient=ctx.createLinearGradient(0,0,canvas.width,0);
-    gradient.addColorStop(0,"magenta");
-    gradient.addColorStop(0.5,"blue");
-    gradient.addColorStop(1.0,"red")
-
-    var random=util.random(0,1);
-    var from=util.random(0,10);
-    var to=util.random(40,50);
-
-    ctx.strokeStyle=gradient;
-    ctx.beginPath();
-    ctx.lineTo(0,random?from:to);
-    ctx.lineTo(100,random?to:from);
-    ctx.stroke();
-
-    /*
-    var stream=canvas.createJPEGStream({
-        bufsize: 2048,
-        quality: 80
-    });
-
-    stream.pipe(res);
-    */
-
-    res.set('Content-Type','text/html');
-    res.end('<img src="'+canvas.toDataURL()+'" />'+result)
-})
+app.get('/captcha',require('./../util/captcha'));
 
 app.use(express.static(__dirname));
 
---
+++ util/captcha.js
@@ -0,0 +1,74 @@
+﻿
+module.exports=function(req,res) {
+
+    var Canvas=require('canvas'),
+        height=50,
+        Image=Canvas.Image,
+        canvas=new Canvas(100,50),
+        ctx=canvas.getContext('2d');
+
+    ctx.fillStyle="#fff";
+    ctx.fillRect(0,0,100,50);
+
+    var util=require('./../core/util');
+
+    var font=['Impact','Arial'];
+    var words=["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z","2","3","4","5","6","7","8","9"];
+
+    var result='';
+    var gradient=ctx.createLinearGradient(0,0,50,0);
+    gradient.addColorStop(0,"magenta");
+    gradient.addColorStop(0.5,"blue");
+    gradient.addColorStop(1.0,"red");
+
+    for(var i=0;i<4;i++) {
+        var fontSize=util.random(35,40);
+        var fontFamily=util.random(0,1);
+        var rotate=.01*util.random(-45,45);
+        var img=new Image;
+        var wordCanvas=new Canvas(50,50);
+        var wordCtx=wordCanvas.getContext('2d');
+        var c=words[util.random(55)];
+
+        result+=c;
+
+        wordCtx.rotate(rotate);
+        wordCtx.font=fontSize+'px '+font[fontFamily];
+
+        // 用渐变进行填充
+        wordCtx.strokeStyle=gradient;
+
+        wordCtx.strokeText(c,rotate>0.15?rotate*50:5,40);
+
+        img.src=wordCanvas.toBuffer();
+
+        ctx.drawImage(img,i*20,0,50,50)
+    }
+
+    gradient=ctx.createLinearGradient(0,0,canvas.width,0);
+    gradient.addColorStop(0,"magenta");
+    gradient.addColorStop(0.5,"blue");
+    gradient.addColorStop(1.0,"red")
+
+    var random=util.random(0,1);
+    var from=util.random(0,10);
+    var to=util.random(40,50);
+
+    ctx.strokeStyle=gradient;
+    ctx.beginPath();
+    ctx.lineTo(0,random?from:to);
+    ctx.lineTo(100,random?to:from);
+    ctx.stroke();
+
+    res.set('Content-Type','image/jpeg');
+    //var stream=canvas.createPNGStream();
+    var stream=canvas.jpegStream({ quality: 90 });
+
+    stream.pipe(res);
+
+    /*
+
+    res.set('Content-Type','text/html');
+    res.end('<img src="'+canvas.toDataURL()+'" />'+result)
+    */
+}
