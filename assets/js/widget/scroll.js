1f20dab93d74886af53d0e0b50593eaf3701904f
sun.lu <sun.lu@Ctrip.com>
2015-06-01 18:09:13 +0800

    fx

--- assets/js/widget/scroll.js
+++ assets/js/widget/scroll.js
@@ -1,114 +1,145 @@
-﻿define(function (require,exports,module) {
+﻿define(function(require,exports,module) {
     var $=require('$'),
         util=require('util'),
         animation=require('animation'),
         ScrollView=require('./scrollview');
 
-    var _start=function (e) {
-        var self=this,
-            point=e.touches[0],
-            matrix=$(self).matrix();
-
-        self._isStart=false;
-        self._isStop=false;
-        self._isMoved=false;
-        self._sy=point.pageY;
-        self._sx=point.pageX;
-        self._st=matrix.ty;
-        self._isLoading=this._isDataLoading;
-    }
-
-    var _move=function (e) {
+    var _start=function(e) {
         var self=this;
 
-        if(self._isStop) return;
+        if(!self.scrollView&&self.parentNode.scrollTop!=0) {
+            self.isStop=true;
+        } else {
+            var point=e.touches[0],
+                matrix=self.$.matrix();
+
+            self.isStart=false;
+            self.isStop=false;
+            self.isMoved=false;
+            self.sy=self.oy=self.pointY=point.pageY;
+            self.sx=point.pageX;
+            self.st=matrix.ty;
+            self.isLoading=self.isDataLoading;
+        }
+    }
+
+    var _move=function(e) {
+        var self=this;
+
+        if(self.isStop) return;
 
         var point=e.touches[0],
-            deltaY=point.pageY-self._sy,
-            deltaX=point.pageX-self._sx,
-            $this=$(self);
+            deltaY=point.pageY-self.sy,
+            deltaX=point.pageX-self.sx;
 
-        if(!self._isStart) {
-            self._isStart=true;
+        self.oy=self.pointY;
+        self.pointY=point.pageY;
+
+        if(!self.isStart) {
+            self.isStart=true;
             if(Math.abs(deltaX)>Math.abs(deltaY)) {
-                self._isStop=true;
+                self.isStop=true;
                 return;
             }
         }
 
-        var scrollView=self.parentNode.__scrollView;
+        var scrollView=self.scrollView;
 
-        if((scrollView?scrollView.y<=0:self.parentNode.scrollTop<=0)&&deltaY>0) {
+        if((scrollView?scrollView.y<=0:self.parentNode.scrollTop==0)&&deltaY>0) {
 
-            if(self.timer) clearTimeout(self.timer),self.timer=null;
+            self.isMoved=true;
+            self.refreshAgain=true;
+            self.ty=self.st+deltaY*.5;
 
-            self._isMoved=true;
-            self._refreshAgain=true;
-            self._ty=self._st+deltaY*.5;
+            self.$.css({ '-webkit-transform': 'translate(0px,'+self.ty+'px) translateZ(0)' });
 
-            $this.css({ '-webkit-transform': 'translate(0px,'+self._ty+'px) translateZ(0)' });
-
-            if(!this._isLoading) {
-                if(self._ty>70) {
-                    self.$refresh.html('释放刷新');
-                } else {
-                    self.$refresh.html('下拉刷新');
-                }
+            if(!this.isLoading) {
+                self.$refresh.html(self.ty>70?'释放刷新':'下拉刷新');
             }
-            return false;
+        } else {
+            self.isStop=true;
         }
+        return self.isStop;
     }
 
-    var _end=function (e) {
+    var _end=function(e) {
         var self=this;
 
-        if(self._isMoved) {
+        if(self.isMoved) {
             var point=e.changedTouches[0],
-                $this=$(self),
-                from=self._ty,
+                from=self.ty,
                 end=from>70?50:0,
-                y;
+                ty,
+                dy=self.oy-point.pageY,
+                bounce=function() {
 
-            self._isMoved=false;
+                    self.$.animate({
+                        '-webkit-transform': 'translate(0px,'+end+'px) translateZ(0)'
 
-            if(self._isLoading&&!self._isDataLoading) {
+                    },300+dy* -1,'cubic-bezier(.3,.78,.43,.95)',function() {
+                        self.refreshAgain=false;
+                        if(!self.isLoading&&end!=0) {
+                            self.isLoading=self.isDataLoading=true;
+                            self.$refresh.html('<div class="dataloading"></div>');
+                            self.$.triggerHandler('refresh');
+                        }
+                    });
+                };
+
+            self.isMoved=false;
+
+            if(self.isLoading&&!self.isDataLoading) {
                 end=0;
             }
 
-            animation.animate(function (d) {
-                y=from+(end-from)*d;
+            if(dy< -5) {
+                self.$.animate({
+                    '-webkit-transform': 'translate(0px,'+(from-dy*1.5)+'px) translateZ(0)'
 
-                $this.css({
-                    '-webkit-transform': 'translate(0px,'+y+'px) translateZ(0)'
-                });
+                },dy* -1.5,'cubic-bezier(.3,.78,.43,.95)',bounce);
+            }
+            else bounce();
 
-            },300,'ease',function () {
-                self._refreshAgain=false;
-                if(!self._isLoading&&end!=0) {
-                    self._isLoading=self._isDataLoading=true;
-                    self.$refresh.html('<div class="dataloading"></div>');
-                    $this.triggerHandler('refresh');
-                }
-            });
 
             return false;
         }
     }
 
-    var touchStart=function (e) {
+    var _refresh=function() {
+        var self=this,
+            complete=function() {
+                self.$refresh.html('下拉刷新');
+                self.isDataLoading=false;
+                if(self.refreshAgain) return;
+
+                var from=self.$.matrix().ty,
+                    end=Math.max(from-50,0);
+
+                self.$.animate({
+                    '-webkit-transform': 'translate(0px,'+end+'px) translateZ(0)'
+                },400,'cubic-bezier(.3,.78,.43,.95)');
+            };
+
+        self.options.refresh(complete,function(error) {
+            sl.tip(error);
+            complete();
+        });
+    };
+
+    var touchStart=function(e) {
         var el=this,
             point=e.touches[0],
             now= +new Date;
 
         el.__sy=el.__pointY=el.__startY=point.pageY;
         el.__sx=point.pageX;
-        el._isMomentum=false;
+        el.__hasMomentum=false;
         el.__isMoved=false;
         el.__isStart=false;
         el.__isScroll=now-el.__timestamp<=16;
     };
 
-    var touchMove=function (e) {
+    var touchMove=function(e) {
         var el=this,
             point=e.touches[0],
             pointY=point.pageY,
@@ -121,72 +152,85 @@
 
         if(!el.__isStart) {
             el.__isStart=true;
-            if(util.android&&!el.options.hScroll&&Math.abs(deltaX)>Math.abs(deltaY)) {
+            if(!el.options.hScroll&&Math.abs(deltaX)>Math.abs(deltaY)) {
                 return false;
             }
         }
+
+        if(util.ios&&el.$refresh&&el.scrollTop<0) {
+            el.isRefresh=el.scrollTop< -70;
+        }
     };
 
-    var scrollStop=function (el) {
+    var scrollStop=function(el) {
         if(el._stm) clearTimeout(el._stm);
-        el._stm=setTimeout(function () {
+        el._stm=setTimeout(function() {
             el._stm=null;
             $(el).trigger('scrollStop',[0,el.scrollTop]);
         },80);
     }
 
-    var scroll=function () {
+    var scroll=function() {
         var el=this;
 
         el.__isScroll=true;
         el.__timestamp= +new Date;
 
-        if(el._isMomentum||util.android) {
+        if(el.__hasMomentum||util.android) {
             scrollStop(el);
         }
     };
 
-    var touchEnd=function (e) {
+    var touchEnd=function(e) {
         var el=this,
             $el=$(el),
-            pointY=e.changedTouches[0].pageY;
+            pointY=e.changedTouches[0].pageY,
+            dy=Math.abs(el.__oPointY-pointY);
 
-        if(util.ios&&Math.abs(el.__oPointY-pointY)<5) {
+        if(util.ios&&dy<5) {
             el._isStop=false;
             scrollStop(el);
-            el._isMomentum=false;
+            el.__hasMomentum=false;
 
         } else
-            el._isMomentum=true;
+            el.__hasMomentum=true;
 
         e.cancelTap=el.__isScroll;
+
+        if(el.isRefresh) {
+            el.$refresh.html('<div class="dataloading"></div>');
+            el.$scroller.css({ '-webkit-transform': 'translate(0px,50px) translateZ(0)' }).triggerHandler('refresh');
+        }
     };
 
-
-    exports.bind=function (selector,options) {
+    exports.bind=function(selector,options) {
         //<--debug
         options={
             useScroll: false,
-            refresh: function (resolve,reject) {
-                setTimeout(function () {
+            refresh: function(resolve,reject) {
+                setTimeout(function() {
                     reject('出错啦');
                 },1000);
             }
         }
-        options.refresh=false;
+        //options.refresh=false;
         //debug-->
 
         var result=[];
 
-        (typeof selector==='string'?$(selector):selector).each(function () {
-            var $el=$(this);
+        (typeof selector==='string'?$(selector):selector).each(function() {
+            var $el=$(this),
+                scrollView;
+
 
             if(options&&options.useScroll||util.android&&parseFloat(util.osVersion<=2.3)) {
-                this.__scrollView=new ScrollView(this,options);
-                result.push(this.__scrollView);
+                scrollView=new ScrollView(this,options);
+                result.push(scrollView);
             }
 
             else {
+                this.__timestamp=0;
+
                 if(util.ios) {
                     $el.css({
                         '-webkit-overflow-scrolling': 'touch',
@@ -201,14 +245,14 @@
                     .on('touchstart',touchStart)
                     .on('touchmove',touchMove)
                     .on('touchend',touchEnd)
-                    .each(function () {
+                    .each(function() {
                         this._scrollTop=0;
                         this.options=$.extend({
                             hScroll: false
                         },options);
                     }),
                     result.push({
-                        destory: function () {
+                        destory: function() {
                             $el.off('touchstart',touchStart)
                                 .off('touchmove',touchMove)
                                 .off('touchend',touchEnd)
@@ -225,39 +269,23 @@
                 if(!$scroller.length) $scroller=ScrollView.addScroller($el);
 
                 var scroller=$scroller[0];
+
                 scroller.$refresh=$refresh;
-                scroller.__timestamp=0;
+                scroller.$=$scroller;
+                scroller.options=options;
+
+                if(scrollView) scroller._scrollView=scrollView;
 
                 $scroller.css({ marginTop: -50 })
                     .prepend($refresh)
                     .on('touchstart',_start)
                     .on('touchmove',_move)
                     .on('touchend',_end)
-                    .on('refresh',function () {
-                        var self=this,
-                            $this=$(self),
-                            complete=function () {
-                                self.$refresh.html('下拉刷新');
-                                self._isDataLoading=false;
-                                if(self._refreshAgain) return;
+                    .on('refresh',_refresh);
 
-                                var from=$(self).matrix().ty,
-                                    end=Math.max(from-50,0);
+                this.$refresh=$refresh;
+                this.$scroller=$scroller;
 
-                                animation.animate(function (d) {
-                                    $this.css({
-                                        '-webkit-transform': 'translate(0px,'+(from+(end-from)*d)+'px) translateZ(0)'
-                                    });
-
-                                },200,'ease',function () {
-                                });
-                            };
-
-                        options.refresh(complete,function (error) {
-                            sl.tip(error);
-                            complete();
-                        });
-                    });
             }
 
         });
