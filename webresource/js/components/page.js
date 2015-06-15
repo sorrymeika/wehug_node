b6b0e4fe8f56a3775f8e946abf6234ca4f1215c9
sun.lu <sun.lu@Ctrip.com>
2015-06-15 18:20:50 +0800

    fx

--- webresource/js/components/page.js
+++ webresource/js/components/page.js
@@ -1,8 +1,12 @@
-﻿define(function (require,exports,module) {
+﻿define(function(require,exports,module) {
     var $=require('$'),
         util=require('util'),
-        format=util.template,
-        bind=util.bind;
+        format=util.template;
+
+    var createButton=function(index,text) {
+        text=text||index;
+        return '<a href="javascript:void(0);" page="'+index+'">'+text+'</a>';
+    }
 
     function Page(options) {
         this.options=$.extend({
@@ -10,7 +14,7 @@
             page: 1,
             pageSize: 10,
             total: 0,
-            onChange: function (page) { },
+            onChange: function(page) { },
             msg: '共<%=total%>条数据&nbsp;&nbsp;每页<%=pageSize%>&nbsp;&nbsp;当前第<%=page%>/<%=totalPages%>页'
         },options);
 
@@ -19,10 +23,10 @@
     };
 
     Page.prototype={
-        draw: function () {
+        draw: function() {
             var options=this.options,
-            container=this.container,
-            pageSize=options.pageSize||10;
+                container=this.container,
+                pageSize=options.pageSize||10;
 
             options.pageSize=pageSize;
             var totalPages=options.totalPages=(options.total-(options.total%pageSize||pageSize))/pageSize+1;
@@ -32,15 +36,14 @@
                 container=$('<div></div>').appendTo(container);
 
                 var html='',
-                page=parseInt(options.page),
-                total=options.total,
-                pageSize=options.pageSize,
-                onChange=options.onChange,
-                oneButton=bind(this._oneButton,this),
-                serialButtons=bind(this._serialButtons,this);
+                    page=parseInt(options.page),
+                    total=options.total,
+                    pageSize=options.pageSize,
+                    onChange=options.onChange,
+                    serialButtons=bind(this._serialButtons,this);
 
                 if(page==1) html+="<span>第一页</span><span>上一页</span>";
-                else html+=oneButton(1,"第一页")+oneButton(page-1,"上一页");
+                else html+=createButton(1,"第一页")+createButton(page-1,"上一页");
 
                 if(totalPages<=7)
                     html+=serialButtons(1,totalPages);
@@ -57,37 +60,33 @@
                     html+=serialButtons(totalPages,totalPages);
                 }
 
-                html+=totalPages==page?'<span>下一页</span><span>最后一页</span>':(oneButton(page+1,"下一页")+oneButton(totalPages,"最后一页"));
+                html+=totalPages==page?'<span>下一页</span><span>最后一页</span>':(createButton(page+1,"下一页")+createButton(totalPages,"最后一页"));
 
                 container.html(html)
-                .find("a")
-                .click(function () {
-                    onChange(this.getAttribute("page"));
-                });
+                    .find("a")
+                    .click(function() {
+                        onChange(this.getAttribute("page"));
+                    });
 
             } else
                 container.html('共0条数据');
         },
-        change: function (opt) {
+        change: function(opt) {
             if(typeof opt=='number')
                 this.options.page=page;
             else
                 $.extend(this.options,opt);
             this.draw();
         },
-        clear: function () {
+        clear: function() {
             this.container.html('&nbsp;');
         },
-        _oneButton: function (index,text) {
-            text=text||index;
-            return format('<a href="javascript:void(0);" page="{0}">{1}</a>',[index,text]);
-        },
-        _serialButtons: function (startPage,endPage) {
-            var currentPage=this.options.page,
-            oneButton=this._oneButton,
+        _serialButtons: function(startPage,endPage) {
+            var currentPage=this.options.page;
+
             html='';
             for(var i=startPage;i<=endPage;i++) {
-                html+=(i==currentPage)?'<span>'+i+'</span>':oneButton(i);
+                html+=(i==currentPage)?'<span>'+i+'</span>':createButton(i);
             }
             return html;
         }
