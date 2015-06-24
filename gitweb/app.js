9a1c629a8eecdb811b16bab185ac0e244fc21d6d
sun.lu <sun.lu@Ctrip.com>
2015-06-24 17:32:28 +0800

    fx

---
+++ gitweb/app.js
@@ -0,0 +1,12 @@
+﻿var gitweb=require('gitweb');
+var path=require('path');
+
+require('http').createServer(gitweb('/',{
+
+    projectroot: path.join(__dirname,"../"),
+
+    max_depth: 2
+
+})).listen(5554,function() {
+    console.log("GitWeb powered by node!\nHTTP server running on port: %d",this.address().port);
+});
