fd281a3ae9bce3af824a50949340f07bf509136c
sorrymeika <sorrymeika@163.com>
2015-07-05 02:07:23 +0800

    fx

--- webresource/js/core/model.js
+++ webresource/js/core/model.js
@@ -1,4 +1,4 @@
-﻿define(function(require,exports,module) {
+﻿define(function (require,exports,module) {
 
     var $=require('$'),
         util=require('util'),
@@ -6,7 +6,7 @@
         Event=require('./event'),
         slice=Array.prototype.slice;
 
-    var setElementProp=function(el,prop,value) {
+    var setElementProp=function (el,prop,value) {
         switch(prop) {
             case 'text':
                 el.innerHTML=util.encodeHTML(value);
@@ -22,47 +22,154 @@
     };
 
     var creator=document.createElement("DIV");
+    var createElement=function (html) {
+        creator.innerHTML=html;
+        return creator.childNodes[0].nodeType==3?creator.childNodes[1]:creator.childNodes[0]
+    }
 
+    var template='<div data-repeat="item in data" class="item">\
+                <img data-binding="src:item.picture,alt:item.alt"/>\
+                <a data-binding="href:item.href">测试<text data-binding="html:item.content"></text>一下</a>\
+            </div>';
+    var el=createElement(template);
+    var now=Date.now();
 
-    var Model=function(data,parent) {
+    console.log(now);
+
+    for(var i=0;i<10000;i++) {
+        createElement(template)
+    }
+    console.log(Date.now()-now);
+
+    for(var i=0;i<10000;i++) {
+        el.cloneNode();
+    }
+    console.log(Date.now()-now);
+
+    var rcollection=/([a-zA-Z_1-9-]+)\s+in\s+([a-zA-Z_1-9-]+(\.[a-zA-Z_1-9-]+){0,})/g;
+
+    var Generator=function ($el) {
+        var repeats={};
+        var collection;
+
+        $el.find('[data-repeat]').each(function () {
+            var el=this;
+            var repeat=this.getAttribute('data-repeat');
+
+            repeat.replace(rcollection,function (match,modelName,collectionName) {
+                var names=collectionName.split('.');
+                var namesLength=names.length;
+
+                if(namesLength!==1) {
+                    var varName=names[0];
+                    var closest=$(el).closest('[data-repeat-model="'+varName+'"]');
+
+                    if(closest.length) {
+                        names[0]=closest.attr('data-repeat-name');
+                        collectionName=names.join('.');
+                    }
+                }
+
+                el.setAttribute('data-repeat-model',modelName);
+                el.setAttribute('data-repeat-name',collectionName);
+
+                var placeHolder=createElement('<script type="text/placeholder"></script>');
+                var placeHolderName;
+                el.parentNode.insertBefore(placeHolder,el);
+                el.parentNode.removeChild(el);
+
+                var listItem={
+                    sort: '',
+                    modelName: modelName,
+                    template: el,
+                    elements: []
+                };
+
+                if(namesLength==1) listItem.placeHolder=placeHolder;
+
+                collection=repeats[collectionName];
+                if(!collection) {
+                    repeats[collectionName]=[listItem];
+                    placeHolderName=collectionName+'_1';
+                } else {
+                    placeHolderName=collectionName+'_'+collection.push(listItem);
+                }
+
+                listItem.placeHolderName=placeHolderName;
+                placeHolder.setAttribute('data-placeholder',placeHolderName);
+            });
+        });
+
+        return repeats;
+    };
+
+    var Model=function (data,hash,parent) {
         if(!data) return;
 
         var model={},
+            value,
             map,
             mapping=this.mapping;
 
-        if(parent) {
-            this.parent=parent;
-        }
-
-        for(var key in data) {
-            map=mapping[key];
-
-            if(typeof map=='function'&&(map.prototype instanceof Model||map.prototype instanceof Collection)) {
-                model[key]=new map(data[key],key,this);
-            } else
-                model[key]=data[key];
-        }
+        if(typeof hash==='object') parent=hash,hash=undefined;
 
         this.model=model;
         this.data=data;
+        this.key=hash||'';
+
+        if(parent) {
+            if(parent instanceof Model) {
+                this.parent=parent;
+                this.key=parent.key+'.'+this.key;
+            } else if(parent instanceof Collection) {
+                this.parent=parent;
+                this.key=parent.key;
+
+            } else {
+                this.generator=parent;
+            }
+        }
+
+        var collection;
+
+        for(var key in data) {
+            map=mapping[key];
+            value=data[key];
+
+            if((map&&map.prototype instanceof Model)||(!map&&$.isPlainObject(value))) {
+                model[key]=new (map||Model)(value,key,this);
+
+            } else if((map&&map.prototype instanceof Collection)||(!map&&$.isArray(value))) {
+                model[key]=collection=new (map||Collection)(value,key,this);
+
+                this.generator&&(collection.list=this.generator[collection.key]);
+
+            } else
+                model[key]=data[key];
+        }
     };
 
     Model.prototype={
+        constructor: Model,
         one: Event.one,
         on: Event.on,
         off: Event.off,
         trigger: Event.trigger,
 
-        bind: function($el,modelName) {
+        generate: function ($el) {
+            this.generator=Generator($el);
+        },
+
+        bind: function ($el,variableName) {
             var self=this;
-            $el.find('[data-binding]').each(function() {
+
+            $el.find('[data-binding]').each(function () {
                 var el=this;
                 var binding=this.getAttribute('data-binding');
-                var rbinding=new RegExp('\\b([a-zA-Z_1-9-]+)\\s*\\:\\s*'+(modelName?modelName+'\\.':'')+'([a-zA-Z_1-9]+)(\s|,|$)','g');
+                var rbinding=new RegExp('\\b([a-zA-Z_1-9-]+)\\s*\\:\\s*'+(variableName?variableName+'\\.':'')+'([a-zA-Z_1-9]+)(\s|,|$)','g');
                 var bindings;
 
-                binding.replace(rbinding,function(match,prop,key) {
+                binding.replace(rbinding,function (match,prop,key) {
                     if(!bindings) {
                         bindings=[el];
                         if(!self.bindings[key]) self.bindings[key]=[];
@@ -73,19 +180,34 @@
                     setElementProp(el,prop,self.data[key]);
                 });
             });
-            //this.$bindings.push.apply(this.$bindings,els);
+
+            for(var key in this.model) {
+                var value=this.model[key];
+                if(value instanceof Model) {
+                    value.bind($el);
+
+                } else if(value instanceof Collection) {
+
+                    for(var i=0,len=value.list.length;i<len;i++) {
+                        var listItem=value.list[i];
+
+                        if(!listItem.placeHolder)
+                            listItem.placeHolder=$el.find('[data-placeholder="'+listItem.placeHolderName+'"]')[0];
+                    }
+
+                }
+            }
+
+            return self;
         },
 
-        get: function(key) {
+        get: function (key) {
             return this.model[key];
         },
 
-        bindings: {/*
-            title: [[el,'src','alt'],[el2,'html']]
-        */
-        },
+        bindings: {},
 
-        _asyncView: function(key,val) {
+        _asyncView: function (key,val) {
 
             var bindings=this.bindings[key],
                 binding,
@@ -107,7 +229,7 @@
             }
         },
 
-        set: function(key,val) {
+        set: function (key,val) {
             var self=this,
                 origin,
                 changed,
@@ -133,54 +255,35 @@
 
         },
 
-        render: function() {
+        render: function () {
         },
 
-        remove: function() {
+        remove: function () {
         },
 
-        save: function() {
+        save: function () {
         }
     };
 
     Model.extend=util.extend;
 
-    var rcollection=/([a-zA-Z_1-9-]+)\s+in\s+([a-zA-Z_1-9-]+(\.[a-zA-Z_1-9-]+){0,})/g;
-
-    var Finder=function($el,data) {
-        $el.find('[data-model]').each(function() {
-            var el=this;
-            var model=this.getAttribute('data-model');
-
-            model.replace(rcollection,function() {
-
-            });
-        });
-    };
-
-    var Collection=function(collection,templates,parent) {
+    var Collection=function (collection,key,parent) {
 
         if(!collection) return;
+        if(typeof key==='object') parent=key,key=undefined;
 
         this.models=[];
         this.data=collection;
-        this.list=[{
-            modelName: 'item',
-            sort: '',
-            $placeHolder: $('<script type="text/placeholder" data-collection="data"></script>'),
-            template: '<div data-model="item in data" class="item">\
-                <img data-binding="src:item.picture,alt:item.alt"/>\
-                <a data-binding="href:item.href">测试<text data-binding="html:item.content"></text>一下</a>\
-            </div>',
-            elements: []
-        }];
 
         if(parent) {
             this.parent=parent;
+            this.key=parent.key+'.'+(hash||'');
+        } else {
+            this.key=hash||'';
         }
 
         for(var i=0,len=collection.length;i<len;i++) {
-            this.add.push(collection[i]);
+            this.add(collection[i]);
         }
         if(parent) this.parent=parent;
     };
@@ -190,7 +293,7 @@
         model: Model,
         url: null,
 
-        forEach: function(fn) {
+        forEach: function (fn) {
             var model;
 
             for(var i=0,len=this.models.length;i<len;i++) {
@@ -200,16 +303,14 @@
             }
         },
 
-        add: function(data,autoAppend) {
+        add: function (data,autoAppend) {
             var model=new Model(data,this);
             this.models.push(model);
 
             for(var i=0,len=this.list.length;i<len;i++) {
                 item=this.list[i];
 
-                creator.innerHTML=item.template;
-
-                var el=creator.childNodes[0].nodeType==3?creator.childNodes[1]:creator.childNodes[0];
+                var el=item.template.cloneNode();
                 var $el=$(el);
 
                 this.parent&&this.parent.bind($el);
@@ -221,28 +322,28 @@
                     item.fragment.appendChild(el);
 
                 } else {
-                    item.$placeHolder.before($model);
+                    item.placeHolder.parentNode.insertBefore(el,item.placeHolder);
                 }
             }
         },
 
-        get: function(i) {
+        get: function (i) {
             return this.models[i];
         },
 
-        remove: function() {
+        remove: function () {
         },
 
-        sort: function() {
+        sort: function () {
         },
 
-        fetch: function() {
+        fetch: function () {
         },
 
-        render: function() {
+        render: function () {
         },
 
-        save: function() {
+        save: function () {
         }
     };
 
@@ -266,7 +367,6 @@
 
         mapping: {
             success: false,
-            data: List,
             msg: ''
         }
     })
@@ -286,22 +386,18 @@
     })
 
 
-    var $el=$('<div data-model="$data"><div data-collection="data" class="list">\
-            <div data-model="data[0]" class="item">\
+    var $el=$('<div data-repeat="$data"><div data-collection="data" class="list">\
+            <div data-repeat="data[0]" class="item">\
                 <img data-binding="src:picture,alt:alt"/>\
                 <div data-binding="innerHTML:data[0].content">asdf</div>\
             </div>\
-            <div data-model="item in data" class="item">\
+            <div data-repeat="item in data" class="item">\
                 <img data-binding="src:item.picture,alt:item.alt"/>\
                 <div data-binding="data:item.content">测试<text data-binding="html:item.content"></text>一下</div>\
             </div>\
         </div></div>').appendTo('body');
 
-    console.log($el.find('[data-binding="data:item.content"]'))
-
     indexModel.bind($el,'item');
-
-    console.log(indexModel.bindings)
 
     var Book=Model.extend({
         id: 'id',
