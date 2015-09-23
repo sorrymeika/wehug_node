fba5d217c967751dd82d611ea955c9d73d467c73
sorrymeika <sorrymeika@163.com>
2015-09-24 02:01:39 +0800

    fx

--- webresource/js/core/viewModel.js
+++ webresource/js/core/viewModel.js
@@ -13,6 +13,17 @@
     var rbinding = /\b([a-zA-Z_0-9-\.]+)\s*\:\s*([a-zA-Z_0-9]+)((?:\.[a-zA-Z_0-9]+)*)((?:\s*\|\s*[a-zA-Z_0-9]+(?:\s*\:\s*\({0,1}\s*(?:[a-zA-Z_0-9\.-]+|'(?:\\'|[^'])*')\){0,1})*)*)(\s|,|$)/g;
     var revents = /\b([a-zA-Z\s]+)\s*\:\s*([a-zA-Z_0-9]+)((?:\.[a-zA-Z_0-9]+)*)((?:\s*\:\s*(?:[a-zA-Z_0-9\.]+|'(?:\\'|[^'])*'))*|\s*=\s*(?:[a-zA-Z_0-9\.]+|'(?:\\'|[^'])*'))(\s|,|$)/g;
     var rmatch = /\{\{(.+?)\}\}/g;
+    var rvar = /(^|[\=\>\<\?\s\:\(\),\%\+\-\*\/\[\]]+)([a-zA-Z_0-9\.-]+|'(?:\\'|[^'])*')/g;
+
+    var isNull = function (str) {
+        var arr = str.split('.');
+        var result = [];
+
+        for (var i = 0; i < arr.length; i++) {
+            result[i] = (i == 0 ? '$data' : result[i - 1]) + '.' + arr[i];
+        }
+        return '(' + result.join('&&') + '?' + str + ':"")';
+    };
 
     var $el = $('<div>\
     <input sn-model="name" />\
@@ -24,7 +35,7 @@
 
 
     for (var i = 0; i < 10000; i++) {
-        $el.append('<div data-id="' + i + '">t</div>')
+        // $el.append('<div data-id="' + i + '">t</div>')
     }
 
     function eachElement(el, fn) {
@@ -36,7 +47,7 @@
             var nodeType = child.nodeType;
             var nodes;
 
-            fn(child, i);
+            fn(child, i, childNodes);
 
             if (nodeType == 1 && (nodes = child.childNodes) && nodes.length) {
                 if (i + 1 < len) {
@@ -81,48 +92,164 @@
         return result;
     }
 
-    function Repeat() {
+    function setElementAttribute(el, model) {
+        if (el.bindings) {
+            for (var attr in el.bindings) {
+                var val = model.root.fns[el.bindings[attr]](model);
+                if (attr == 'textContent')
+                    el.textContent = val;
+                else
+                    el.setAttribute(attr, val);
+            }
+        }
     }
 
-    Repeat.prototype.value = function () {
+    var cloneNode = function (el, model) {
+        var node = el.cloneNode();
+        var len;
+
+        if (el.bindings) {
+            setElementAttribute(el, model);
+            el.elements[el.elements.length] = node;
+        }
+        if (el.nodeType == 1 && (len = el.childNodes.length)) {
+            for (var i = 0; i < len; i++) {
+                node.appendChild(cloneNode(el.childNodes[i]));
+            }
+        }
+        return node;
     }
 
-    function ViewModel(template) {
-        this.data = {};
+    function compileExpression(expression, repeat) {
+        var code = 'function(model){var $data=$.extend({},model.root.data';
+
+        if (repeat) {
+            code += ',{';
+
+            var parent = repeat.parent;
+            while (parent) {
+                code += parent.alias + ':model.closest("' + parent.collectionName + '^child").data'
+            }
+            code += repeat.alias + ':model.data}';
+        }
+        code += ');with($data){return \''
+            + expression.replace(/\\/g, '\\\\')
+             .replace(/'/g, '\\\'')
+             .replace(rmatch, function (match, exp) {
+                 return '\'+' + exp.replace(/\\'/, '\'').replace(rvar, function (match, prefix, name) {
+                     return prefix + isNull(name);
+                 }) + '+\'';
+             });
+
+        code += '\';}}';
+
+        return code;
+    }
+
+    var Filter = {};
+
+    function Repeat(options) {
+        $.extend(this, options);
+
+        var attrs = this.collectionName.split('.');
+        var parent = this.parent;
+        while (parent) {
+            if (parent.alias == attrs[0]) {
+                attrs[0] = parent.collectionName;
+                this.collectionName = attrs.join('.') + '^child'
+                break;
+            }
+        }
+        this.elements = [];
+
+        console.log('collectionName:', this.collectionName);
+    }
+
+    Repeat.prototype.update = function () {
+    }
+
+    Repeat.prototype.add = function (model) {
+        this.elements[this.elements.length] = cloneNode(this.el);
+    }
+
+
+    function ViewModel(template, data) {
+        this.data = $.extend({}, data);
+        this.fns = [];
+
         this.scan(template);
     }
 
     ViewModel.prototype = Object.create(Event);
 
+    ViewModel.prototype.bindAttr = function (node, attr, expression, repeat) {
+        var self = this;
+        self.root = this;
+
+        if (!rmatch.test(expression)) return;
+
+        var code = compileExpression(expression, repeat, function () {
+        });
+
+        (node.bindings || (node.elements = [], node.bindings = {}))[attr] = self.fns.length;
+        self.fns.push(code);
+    }
+
     ViewModel.prototype.scan = function (el) {
         var self = this;
+        var elements = [];
 
-        eachElement(el, function (child, i) {
+        eachElement(el, function (child, i, childList) {
             if (child.nodeType == 1) {
                 var repeat = child.getAttribute('sn-repeat');
                 if (repeat != null) {
-                    var match = repeat.match(rrepeat),
-                        itemAlias = match[1],
-                        indexAlias = match[2],
-                        collectionName = match[3],
-                        filters = match[4],
-                        orderBy = match[5];
+                    var match = repeat.match(rrepeat);
+                    var replacement = document.createComment('');
 
-                    console.log(match);
+                    repeat = new Repeat({
+                        parent: childList.repeat,
+                        alias: match[1],
+                        indexAlias: match[2],
+                        collectionName: match[3],
+                        filters: match[4],
+                        orderBy: match[5],
+                        replacement: replacement,
+                        el: child
+                    });
+                    child.parentNode.insertBefore(replacement, child);
+                    child.parentNode.removeChild(child);
+
+                } else {
+                    repeat = childList.repeat;
                 }
+                child.childNodes.repeat = repeat;
 
                 for (var j = 0; j < child.attributes.length; j++) {
                     var attr = child.attributes[j].name;
                     var val = child.attributes[j].value;
 
-                    if (attr != 'sn-repeat' && rmatch.test(val)) {
-                        //function(model){ with(model.root){} };
-
-                        self.on('change:')
+                    if (attr.indexOf('sn-') != 0) {
+                        self.bindAttr(child, attr, val, repeat);
                     }
+                }
+                if (!repeat && child.bindings) {
+                    elements[elements.length] = child;
+                }
+
+            } else if (child.nodeType == 3) {
+                self.bindAttr(child, 'textContent', child.textContent, childList.repeat);
+                if (!childList.repeat && child.bindings) {
+                    elements[elements.length] = child;
                 }
             }
         });
+
+        console.log('[' + this.fns.join(',') + ']');
+        this.fns = eval('[' + this.fns.join(',') + ']');
+
+        for (var i = 0, len = elements.length; i < len; i++) {
+            setElementAttribute(elements[i], self);
+        }
     }
 
     var now = Date.now();
@@ -132,43 +259,12 @@
     console.log(Date.now() - now);
 
     now = Date.now();
-    var vm = new ViewModel($el);
+    var vm = new ViewModel($el, {
+        name: 'tets'
+    });
 
     console.log(Date.now() - now);
 
-    now = Date.now();
-    var vm = new ViewModel($el);
-
-    console.log(Date.now() - now);
-
-    now = Date.now();
-    var vm = new ViewModel($el);
-
-    console.log(Date.now() - now);
-
-
-
-    now = Date.now();
-    var vm = new ViewModel($el);
-
-    console.log(Date.now() - now);
-
-    now = Date.now();
-
-    $el.find('[data-id="1000"]')
-    $el.find('[data-id="2000"]')
-    $el.find('[data-id="3000"]')
-    $el.find('[data-id="3000"]')
-    $el.find('[data-id="3000"]')
-    $el.find('[data-id="3000"]')
-    $el.find('[data-id="3000"]');
-
-    $el.find('[data-id="5000"]');
-    $el.find('[data-id="5000"]');
-    $el.find('[data-id="5000"]');
-    $el.find('[data-id="5000"]');
-
-    console.log(Date.now() - now);
 
     /*
     function testCollectionItem() {
